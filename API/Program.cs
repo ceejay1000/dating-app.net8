473309453dedc156528313f7cb6aa3a11f5d3268
ceejay_original <emmanuelcudjoe477@gmail.com>
2024-07-19 15:43:07 +0000

    Added Angular client

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // app.UseAuthorization();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
