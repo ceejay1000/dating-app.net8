04bedff60726cdb6ee3fde96963e5f846f424283
ceejay_original <emmanuelcudjoe477@gmail.com>
2024-07-22 01:22:00 +0000

    Added routing to client

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,16 +2,27 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountsService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
@@ -20,12 +31,17 @@ export class NavComponent {
       next: (response) => {
         console.log(response);
         // this.accountService.()
+        void this.router.navigateByUrl('/members');
       },
-      error: (err) => console.log(err),
+      error: (err) => {
+        console.log(err);
+        this.toastr.error(err.error);
+      },
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, input, Input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../_models/User';
 import { AccountsService } from '../_services/accounts.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -15,6 +16,7 @@ export class RegisterComponent {
   // usersFromHomeComponent: any = input.required<any>();
   // @Output() cancelRegister = new EventEmitter<boolean>();
   private accountService = inject(AccountsService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
 
@@ -23,6 +25,10 @@ export class RegisterComponent {
       next: (response) => {
         console.log(response);
         this.cancel();
+      },
+      error: (err) => {
+        console.log(err);
+        this.toastr.error(err.error);
       },
     });
   }
